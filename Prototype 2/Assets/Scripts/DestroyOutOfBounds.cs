7795f007613d4fffd8e13bde2fd31def38360b23
Svenlaa <sven.lammertink@hotmail.com>
2023-01-04 11:29:45 +0100

    Add Prototype 2

---
+++ Prototype 2/Assets/Scripts/DestroyOutOfBounds.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class DestroyOutOfBounds : MonoBehaviour
+{
+    private float upperBound = 30;
+    private float lowerBound = -10;
+
+    private void Update()
+    {
+        if (transform.position.z > upperBound)
+            Destroy(gameObject);
+        else if (transform.position.z < lowerBound)
+        {
+            Destroy(gameObject);
+            Debug.Log("Animal Died");
+        }
+    }
+}
---
+++ Prototype 2/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] animalPrefabs;
+    private float spawnRangeX = 20;
+    private float spawnPosZ = 25;
+
+
+    private float startDelay = 2;
+    private float interval = 1.5f;
+
+    private void Start()
+    {
+        InvokeRepeating(nameof(SpawnRandomAnimal), startDelay, interval);
+    }
+
+    private void SpawnRandomAnimal()
+    {
+        var animalIndex = Random.Range(0, animalPrefabs.Length);
+        var animal = animalPrefabs[animalIndex];
+        var spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+
+        Instantiate(animal, spawnPos, animal.transform.rotation);
+    }
+}
