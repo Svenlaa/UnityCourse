3f4a60fb1c19fb5f792b9a0f56d2c3bc288f6963
Svenlaa <sven.lammertink@hotmail.com>
2023-01-07 23:42:52 +0100

    Revert "Add Bonus 2"
    This reverts commit 9ba12053751a3125b5a5cf342f0e35cc0b63eaba.

--- Prototype 2/Assets/Scripts/PlayerController.cs
+++ Prototype 2/Assets/Scripts/PlayerController.cs
@@ -1,13 +1,10 @@
 ﻿using UnityEngine;
-using UnityEngine.Serialization;
 
 public class PlayerController : MonoBehaviour
 {
     public float horizontalInput;
-    public float verticalInput;
     public float speed = 10.0f;
     public float xRange = 10.0f;
-    public float zUpper = 4.0f;
     public GameObject projectilePrefab;
 
 
@@ -18,17 +15,10 @@ public class PlayerController : MonoBehaviour
         if (transform.position.x > xRange)
             transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
 
-        if (transform.position.z > zUpper)
-            transform.position = new Vector3(transform.position.x, transform.position.y, zUpper);
-        if (transform.position.z < 0)
-            transform.position = new Vector3(transform.position.x, transform.position.y, 0);
-
         if (Input.GetKeyDown(KeyCode.Space))
             Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
 
         horizontalInput = Input.GetAxis("Horizontal");
-        verticalInput = Input.GetAxis("Vertical");
         transform.Translate(Vector3.right * (horizontalInput * Time.deltaTime * speed));
-        transform.Translate(Vector3.forward * (verticalInput * Time.deltaTime * speed));
     }
 }
