b552abf7b52fe1565dd98bc989ee0d7199c5f268
Svenlaa <sven.lammertink@hotmail.com>
2023-01-16 10:28:05 +0100

    Update Prototype 3 - Add Animations

--- Prototype 3/Assets/Scripts/PlayerController.cs
+++ Prototype 3/Assets/Scripts/PlayerController.cs
@@ -3,22 +3,28 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRb;
+    private Animator playerAnim;
     public float jumpForce = 10;
     public float gravityModifier = 1;
     public bool isOnGround = true;
     public bool gameOver;
+    private static readonly int JumpTrig = Animator.StringToHash("Jump_trig");
+    private static readonly int DeathB = Animator.StringToHash("Death_b");
+    private static readonly int DeathTypeINT = Animator.StringToHash("DeathType_int");
 
     private void Start()
     {
         playerRb = GetComponent<Rigidbody>();
+        playerAnim = GetComponent<Animator>();
         Physics.gravity *= gravityModifier;
     }
 
     private void Update()
     {
-        if (!Input.GetKeyDown(KeyCode.Space) || !isOnGround) return;
+        if (gameOver || !Input.GetKeyDown(KeyCode.Space) || !isOnGround) return;
 
         playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        playerAnim.SetTrigger(JumpTrig);
         isOnGround = false;
     }
 
@@ -28,6 +34,8 @@ public class PlayerController : MonoBehaviour
             isOnGround = true;
         else if (collision.gameObject.CompareTag("Obstacle"))
         {
+            playerAnim.SetInteger(DeathTypeINT, 1);
+            playerAnim.SetBool(DeathB, true);
             Debug.Log("Game Over!");
             gameOver = true;
         }
