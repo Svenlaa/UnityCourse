e8b45271678acdbd5012f5b486f298e56a7a4310
Svenlaa <sven.lammertink@hotmail.com>
2023-01-23 23:30:26 +0100

    Add Lab 3

---
+++ Personal Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float horizontalInput;
+    private float speed = 10.0f;
+    private float xRange = 7;
+
+    private void Update()
+    {
+        MovePlayer();
+        ConstrainPlayerPosition();
+    }
+
+    void MovePlayer()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * (speed * Time.deltaTime * horizontalInput));
+    }
+
+    void ConstrainPlayerPosition()
+    {
+        Vector3 pos = transform.position;
+
+        if (pos.x > xRange)
+            transform.position = new Vector3(xRange, pos.y, pos.z);
+
+        else if (pos.x < -xRange)
+            transform.position = new Vector3(-xRange, pos.y, pos.z);
+    }
+}
